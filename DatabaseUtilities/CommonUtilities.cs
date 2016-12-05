e5ef0d25a7261f7b474451b57c71dcf584a0a46e
Matthew Cannon <wizard07ksu@gmail.com>
2016-12-04 18:50:50 -0600

    Added an overload of the Convert function and documentation.
    The new overloads accepts a default argument to use instead of the implementation defined default.

--- DatabaseUtilities/CommonUtilities.cs
+++ DatabaseUtilities/CommonUtilities.cs
@@ -8,11 +8,53 @@ namespace Cannon.DatabaseUtilities
 {
     public static class CommonUtilities
     {
+        /// <summary>
+        /// Converts a value from a database into the provided type. Will return
+        /// the default value of T if <paramref name="aValue"/> is <see cref="DBNull"/>.
+        /// </summary>
+        /// <typeparam name="T">
+        /// The datatype to convert to.
+        /// </typeparam>
+        /// <param name="aValue">
+        /// The value to convert.
+        /// </param>
+        /// <returns>
+        /// An instance of T that represents the provided argument.
+        /// </returns>
         public static T Convert<T>( object aValue )
         {
             if ( aValue is System.DBNull )
             {
                 return default( T );
+            }
+            else
+            {
+                return (T)aValue;
+            }
+        }
+
+        /// <summary>
+        /// Converts a value from a database into the provided type. Will return
+        /// the provided default argument value if <paramref name="aValue"/> is 
+        /// <see cref="DBNull"/>.
+        /// </summary>
+        /// <typeparam name="T">
+        /// The datatype to convert to.
+        /// </typeparam>
+        /// <param name="aValue">
+        /// The value to convert.
+        /// </param>
+        /// <param name="aDefault">
+        /// The default value to return if <paramref name="aValue"/> is null.
+        /// </param>
+        /// <returns>
+        /// An instance of T that represents the provided argument.
+        /// </returns>
+        public static T Convert<T>( object aValue, T aDefault )
+        {
+            if ( aValue is System.DBNull )
+            {
+                return aDefault;
             }
             else
             {
