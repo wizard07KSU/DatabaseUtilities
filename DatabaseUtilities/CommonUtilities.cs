db03ef41911d34c1e8f3534f0242599f95226fac
Matthew Cannon <wizard07ksu@gmail.com>
2016-12-04 18:46:57 -0600

    Added utilities class for holding common functionality.
    Currently only has one function to make it easier to read nullable values from a database.

---
+++ DatabaseUtilities/CommonUtilities.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cannon.DatabaseUtilities
+{
+    public static class CommonUtilities
+    {
+        public static T Convert<T>( object aValue )
+        {
+            if ( aValue is System.DBNull )
+            {
+                return default( T );
+            }
+            else
+            {
+                return (T)aValue;
+            }
+        }
+    }
+}
